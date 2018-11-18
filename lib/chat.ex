0e8e2e19a2bb2245b9a27d00167673489caf041a
sophiedebenedetto <sophie.debenedetto@gmail.com>
2018-11-18 13:01:42 -0500

    moebi feature + post draft

--- lib/chat.ex
+++ lib/chat.ex
@@ -2,40 +2,36 @@ defmodule Chat do
   @moduledoc """
   Documentation for Chat.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Chat.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
-  def send_message(recipient, message) do
-    __MODULE__.spawn_task(__MODULE__, :receive_message, recipient, [message])
-  end
-
   def receive_message(message) do
     IO.puts message
   end
 
-  def spawn_task(module, fun, recipient, args, env \\ Mix.env) do
-    do_spawn_task({module, fun, recipient, args}, env)
+  def receive_message_for_moebi(message, from) do
+    IO.puts message
+    send_message(from, "chicken?")
+  end
+
+  def send_message(:moebi@localhost, message) do
+    spawn_task(__MODULE__, :receive_message_for_moebi, :moebi@localhost, [message, Node.self()])
+  end
+
+  def send_message(recipient, message) do
+    spawn_task(__MODULE__, :receive_message, recipient, [message])
+  end
+
+  def spawn_task(module, fun, recipient, args) do
+    Task.Supervisor.async(remote_supervisor(recipient), module, fun, args)
+    |> Task.await
   end
 
   # defp do_spawn_task({module, fun, args}, :test) do
   #   apply(module, fun, args)
   # end
 
-  defp do_spawn_task({module, fun, recipient, args}, _) do
-    Task.Supervisor.async(remote_supervisor(recipient), module, fun, args)
-    |> Task.await
-  end
+  # defp do_spawn_task({module, fun, recipient, args}, _) do
+  #   Task.Supervisor.async(remote_supervisor(recipient), module, fun, args)
+  #   |> Task.await
+  # end
 
   defp remote_supervisor(recipient) do
     {Chat.TaskSupervisor, recipient}
